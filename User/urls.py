db9f069164fe807eff27feee64f16557b25b56ab
nelson chege <nelson3chege@gmail.com>
2022-01-07 10:54:02 +0300

    add comments

--- User/urls.py
+++ User/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import hello_world,ListUsers,ListSubUsers
 urlpatterns = [
     path('hello', hello_world),
+    # used to get and post a new mainuser into db
     path('ListUsers/', ListUsers.as_view()),
+    # used to get a specific main user to update or delete
     path('ListUsers/<int:pk>', ListUsers.as_view()),
+    # used to post a new subuser into db
     path('ListSubUsers/', ListSubUsers.as_view()),
+    # used to get a specific subusers for a main user using the mainusers_id
     path('ListSubUsers/<int:pk>', ListSubUsers.as_view()),
 ]
